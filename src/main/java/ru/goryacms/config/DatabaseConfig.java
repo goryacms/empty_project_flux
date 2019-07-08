481d350a9a42507bbbf55df049fe2889ac069a22
maxim.goryachev <maxim.goryachev@haulmont.com>
2019-07-08 17:32:32 +0300

    Added

--- src/main/java/ru/goryacms/config/DatabaseConfig.java
+++ src/main/java/ru/goryacms/config/DatabaseConfig.java
@@ -6,41 +6,44 @@ import io.r2dbc.spi.ConnectionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.r2dbc.function.DatabaseClient;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 import org.springframework.data.r2dbc.repository.support.R2dbcRepositoryFactory;
 import org.springframework.data.relational.core.mapping.RelationalMappingContext;
+import ru.goryacms.user.repository.UserRepository;
 
 @Configuration
+@EnableR2dbcRepositories
 public class DatabaseConfig {
     @Bean
     PostgresqlConnectionFactory connectionFactory() {
         return new PostgresqlConnectionFactory(
                 PostgresqlConnectionConfiguration.builder()
-                .host("192.168.99.100")
-                .database("webflux")
-                .username("postgres")
-                .password("goryacms")
-                .build();
+                        .host("192.168.99.100")
+                        .port(5432)
+                        .database("webflux")
+                        .username("postgres")
+                        .password("goryacms")
+                        .build()
         );
     }
 
     @Bean
-    DatabaseClient databaseClient(ConnectionFactory connectionFactory){
+    DatabaseClient databaseClient(ConnectionFactory connectionFactory) {
         return DatabaseClient.builder()
                 .connectionFactory(connectionFactory)
                 .build();
     }
 
     @Bean
-    R2dbcRepositoryFactory repositoryFactory(DatabaseClient client){
+    R2dbcRepositoryFactory repositoryFactory(DatabaseClient client) {
         RelationalMappingContext context = new RelationalMappingContext();
         context.afterPropertiesSet();
         return new R2dbcRepositoryFactory(client, context);
     }
 
-    // TODO: after inserting MVC-layouts
-//    @Bean
-//    MyRepository myRepository(R2dbcRepositoryFactory factory){
-//        return factory.getRepository(MyRepository.class);
-//    }
+    @Bean
+    UserRepository myRepository(R2dbcRepositoryFactory factory){
+        return factory.getRepository(UserRepository.class);
+    }
 
 }
